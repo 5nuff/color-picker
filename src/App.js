7a2e297b8e7565cbba25d52d8f9ac593971ddd98
5nuff <intechnic.l.r@gmail.com>
2019-11-20 09:41:37 -0500

    color-picker

--- src/App.js
+++ src/App.js
@@ -1,10 +1,64 @@
-import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import React, { useState } from 'react'
 
-class App extends Component {
-  render() {
-    return <HelloWorld />
-  }
+const App = () => {
+  const [hue, setHue] = useState()
+  const [saturation, setSat] = useState()
+  const [lightness, setLight] = useState()
+  return (
+    <>
+      <main>
+        <h1>
+          <header>Color Picker</header>
+          <div
+            className="background"
+            style={{
+              backgroundColor: `hsl(${hue},${saturation}%,${lightness}%)`,
+            }}
+          ></div>
+        </h1>
+        <section className="controls">
+          <div className="sliders">
+            <div className="h-slider">
+              <input
+                type="range"
+                min="0"
+                max="360"
+                onChange={event => {
+                  setHue(event.target.value)
+                }}
+              />
+              <p>H</p>
+            </div>
+            <div className="s-slider">
+              <input
+                type="range"
+                min="0"
+                max="100"
+                onChange={event => {
+                  setSat(event.target.value)
+                }}
+              />
+              <p>S</p>
+            </div>
+            <div className="l-slider">
+              <input
+                type="range"
+                min="0"
+                max="100"
+                onChange={event => {
+                  setLight(event.target.value)
+                }}
+              />
+              <p>L</p>
+            </div>
+          </div>
+          <div className="hsl">
+            hsl({hue}, {saturation}, {lightness})
+          </div>
+        </section>
+      </main>
+    </>
+  )
 }
 
 export default App
